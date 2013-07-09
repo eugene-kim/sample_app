08d453337967d9f99bc8285832dc05fcd1f2ca8c
youj3an <youj3an@gmail.com>
2013-07-09 02:13:46 -0700

    Finish the signup form

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      "Eugene Kim"
+    email     "youj3an@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -63,7 +63,7 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "email address with mixed case" do
+  describe "email address with mixed case"  do
     let (:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
     it "should be saved as all lower-case" do
